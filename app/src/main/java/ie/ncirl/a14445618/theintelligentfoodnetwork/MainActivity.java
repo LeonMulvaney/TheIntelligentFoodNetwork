006b242f32ed5280d96e557d0f7f079a9eb40c96
LeonMulvaney <leonmul96@gmail.com>
2017-11-17 15:55:27 +0000

    Friday 17/11/2017 @ 15.55 New Logo Design in Gimp Addition of Splash Screen MainActivity is now changed to Home

--- app/src/main/java/ie/ncirl/a14445618/theintelligentfoodnetwork/MainActivity.java
+++ app/src/main/java/ie/ncirl/a14445618/theintelligentfoodnetwork/MainActivity.java
@@ -1,20 +1,36 @@
 package ie.ncirl.a14445618.theintelligentfoodnetwork;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int DELAY_MILLISECONDS = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //From https://stackoverflow.com/questions/8456835/how-to-disable-action-bar-permanently
+        //Hide Action Bar On Splash Screen --------------------------------
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //SplashScreen from Lecture Notes Dr Anu Sahni--------------
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Home.class);
+                startActivity(i);
+                finish();
+            }
+        }, DELAY_MILLISECONDS);
 
-    public void openMyFoodNetwork(View view){
-        Intent intent = new Intent(this,MyFoodNetwork.class);
-        startActivity(intent);
+
     }
 }
