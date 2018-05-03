1353e81520b348faf9989c1542d70c5c3064c802
LeonMulvaney <leonmul96@gmail.com>
2018-05-03 16:51:35 +0100

    Thursday 03/05/2018 @ 16.52 Hamburger Menu Started

--- app/src/main/java/ie/ncirl/a14445618/theintelligentfoodnetwork/Shopping.java
+++ app/src/main/java/ie/ncirl/a14445618/theintelligentfoodnetwork/Shopping.java
@@ -3,6 +3,8 @@ package ie.ncirl.a14445618.theintelligentfoodnetwork;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -28,6 +30,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Shopping extends AppCompatActivity {
+
+    //Hamburger Menu
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
     DatabaseReference shoppingListRef;
@@ -40,6 +49,9 @@ public class Shopping extends AppCompatActivity {
         setTitle(R.string.shopping);
         //Add Back Button to Action Bar - From https://stackoverflow.com/questions/12070744/add-back-button-to-action-bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+
         setContentView(R.layout.activity_shopping);
 
         //Firebase
@@ -47,13 +59,39 @@ public class Shopping extends AppCompatActivity {
         databaseReference = firebaseDatabase.getReferenceFromUrl("https://theintelligentfoodnetwork.firebaseio.com/");
         shoppingListRef = databaseReference.child("ShoppingList");
 
+        //Hamburger Menu
+        mDrawerList = findViewById(R.id.navList);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+        mDrawerLayout = findViewById(R.id.shoppingActivityLayout);
+
+        addDrawerItems();
+        setupDrawer();
     }
 
     //Function to return when back button is pressed From --> Same link as "Add Back Button" above
     @Override
-    public boolean onSupportNavigateUp(){
-        finish();
-        return true;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     //Android Snackbar From: https://spin.atomicobject.com/2017/07/10/android-snackbar-tutorial/
@@ -70,6 +108,37 @@ public class Shopping extends AppCompatActivity {
     public void openShoppingListRecommendation(View view){
         Intent intent = new Intent(this,ShoppingListRecommendation.class);
         startActivity(intent);
+    }
+
+    //Hamburger Menu
+    private void addDrawerItems() {
+        String[] array = { "Home", "Food Network", "Shopping", "Recipes", "Account" };
+        mAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, array);
+        mDrawerList.setAdapter(mAdapter);
+    }
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
     }
 
 
